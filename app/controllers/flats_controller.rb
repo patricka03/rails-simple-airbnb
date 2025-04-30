bb75de2a8175878742a12433188d101cbe7a0de2
Patrick Amfo <patrick.amfo@icloud.com>
2025-04-30 13:13:14 +0100

    added destroy edit and update to the controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -20,6 +20,21 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(strong_params)
+    redirect_to flats_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat = Flat.destroy
+  end
+
   private
 
   def strong_params
